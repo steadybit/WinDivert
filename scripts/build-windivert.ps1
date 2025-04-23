60a8eb8d73a7dfd298c11c56e1aad2dfaeaba292
AleksaBajat <aleksabajat15@gmail.com>
2025-04-23 17:06:42 +0200

    feat: optional signing in the pipeline

--- scripts/build-windivert.ps1
+++ scripts/build-windivert.ps1
@@ -3,17 +3,22 @@ Set-Location -Path (Join-Path $PSScriptRoot "..")
 
 if ($args.Count -eq 0) {
     $config = "Debug"
-} else {
+    $sign = "NoSign"
+} elseif ($args.Count -eq 1) {
     $config = $args[0]
+    $sign = "NoSign"
+}else{
+    $config = $args[0]
+    $sign = $args[1] 
 }
 
-if ($config -match "Debug") {
+if($sign -match "Sign"){
     Write-Host "Creating self-signing certificate"
     $cert = New-SelfSignedCertificate -Type Custom -Subject "CN=WinDivertTestCert" -KeyUsage DigitalSignature -FriendlyName "WinDivert Test Cert" -CertStoreLocation "Cert:\CurrentUser\My"
     Write-Output $cert
     Write-Host "Building $config driver using self-signing certificate"
     msbuild "sys\windivertdriver.vcxproj" /v:n /p:Configuration=$config /p:Platform=x64 /p:TestCertificate=$($cert.Thumbprint) /p:OutDir="..\output\x64\$config\"
-} else {
+}else{
     Write-Host "Building $config driver"
     msbuild "sys\windivertdriver.vcxproj" /v:n /p:Configuration=$config /p:Platform=x64 /p:OutDir="..\output\x64\$config\" /p:SignMode=Off
 }
